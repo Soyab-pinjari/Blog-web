21e4f449ca8676ed24f4f2e5cfc861bf995b4c54
Parvez _ its <Parvejpinjari007@gmail.com>
2026-08-24 11:50:43 +0530

    animation home page

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,25 +1,59 @@
 import React from 'react'
 import { Route } from 'react-router'
+import { motion } from 'framer-motion'
 import Navbar from '../Component/Navbar'
 import Hero from '../Pages/Hero'
 import FeaturedBlog from '../Pages/FeaturedBlog'
 import Footer from '../Component/Footer'
 import About from '../Pages/About'
 
-
+const fadeUp = {
+  hidden: { opacity: 0, y: 40 },
+  visible: (i = 1) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: i * 0.15, duration: 0.6, ease: 'easeOut' },
+  }),
+}
 
 function MainLayout() {
   return (
     <div>
-    <Navbar/>
-    <Hero/>
-    {/* <Steps/> */}
-    <FeaturedBlog/>
-    <section id='aboutPage'>
+      <Navbar />
 
-    <About/>
-    </section>
-    <Footer/>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={fadeUp}
+        custom={0}
+      >
+        <Hero />
+      </motion.div>
+
+      {/* <Steps/> */}
+
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeUp}
+        custom={1}
+      >
+        <FeaturedBlog />
+      </motion.div>
+
+      <motion.section
+        id="aboutPage"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeUp}
+        custom={2}
+      >
+        <About />
+      </motion.section>
+
+      <Footer />
     </div>
   )
 }
