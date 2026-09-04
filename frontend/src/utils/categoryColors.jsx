91a55772032ba59e38a0600c3738e04f80971db8
Parvez _ its <Parvejpinjari007@gmail.com>
2026-09-04 12:17:15 +0530

    ss

--- frontend/src/utils/categoryColors.jsx
+++ frontend/src/utils/categoryColors.jsx
@@ -2,7 +2,7 @@ export const categoryColors = {
   technology: "bg-blue-600",
   travel: "bg-green-600",
   education: "bg-yellow-500",
-  health: "bg-red-500",
+  "health-fitness" : "bg-red-500",
   business: "bg-purple-600",
   programming: "bg-indigo-600",
   lifestyle: "bg-pink-500",
